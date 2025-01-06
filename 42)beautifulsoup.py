974802a5d7592f0f2bbf8fe576e0287be1756aef
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-01-06 20:57:05 +0300

    Add files via upload

---
+++ 42)beautifulsoup.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup as bs 
+import requests
+# from parse import *
+# import time
+# import random
+# for i in range(1,10):
+#     adres=f'https://www.lcw.com/kadin-tisort-t-348?sayfa={i}'
+#     baslik={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+#     sayfa=requests.get(adres,headers=baslik)
+#     soup=bs(sayfa.content,features='lxml')
+#     kadın=soup.find_all('img',{'class':'product-image__image'})
+#     time.sleep(random.randint(3,8))
+#     for foto in kadın:
+#         print(foto)
+
+
